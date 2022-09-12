4d2c5012e2b242c62033aae1cabc8eb59487f18d
gusradika <aditezo24@gmail.com>
2022-09-12 13:15:06 +0700

    test

---
+++ Latihan14-Upload/functions.php
@@ -0,0 +1,52 @@
+<?php
+
+// 1 Connect
+// P4 parameter. 1.Host. 2.Username. 3.Password, 4.Table;
+$conn = mysqli_connect("localhost", "root", "", "latihan");
+
+function query($query)
+{
+    global $conn;
+    $rows = [];
+    // 2 Query
+    $result = mysqli_query($conn, $query);
+    // 3 to Array Associative
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    };
+    return $rows;
+};
+
+
+function tambahdata($data)
+{
+
+    global $conn;
+    $nama = htmlspecialchars($data['nama']);
+    $email = htmlspecialchars($data['email']);
+    $jurusan = htmlspecialchars($data['jurusan']);
+    $gambar = htmlspecialchars($data['gambar']);
+    $alamat = htmlspecialchars($data['alamat']);
+    $query = "INSERT INTO mahasiswa VALUES ('','$nama','$email','$jurusan','$alamat','$gambar')";
+    mysqli_query($conn, $query);
+
+    return (mysqli_affected_rows($conn));
+}
+
+function hapus($id)
+{
+    global $conn;
+    $query = "DELETE FROM mahasiswa WHERE id = $id";
+
+    mysqli_query($conn, $query);
+
+    return (mysqli_affected_rows($conn));
+}
+
+function cari($keyword)
+{
+    global $conn;
+    $query = "SELECT * FROM mahasiswa WHERE nama LIKE '%$keyword%'OR jurusan LIKE '%$keyword%';";
+
+    return (query($query));
+}
---
+++ Latihan14-Upload/index.php
@@ -0,0 +1,62 @@
+<?php
+
+require("functions.php");
+$query = "SELECT * FROM mahasiswa";
+
+$data = query($query);
+
+if (isset($_POST['cari'])) {
+    $data = cari($_POST['keyword']);
+}
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan DataBase 3</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <h1>Data Mahasiswa</h1>
+    <form action="" method="POST">
+        <input type="text" name="keyword" id="" placeholder="Pencarian..." size="30" autofocus>
+        <button type="submit" name="cari">cari</button>
+    </form>
+
+
+    <a href="tambah.php">
+        <h3>Tambah Data Mahasiswa</h3>
+    </a>
+    <table cellspacing="0" cellpadding="20" border="1">
+        <tr>
+            <th>id</th>
+            <th>Aksi</th>
+            <th>gambar</th>
+            <th>nama</th>
+            <th>email</th>
+            <th>jurusan</th>
+            <th>alamat</th>
+        </tr>
+        <?php foreach ($data as $mhs) : ?>
+            <tr>
+                <td><?= $mhs['id'] ?></td>
+                <td><a href="hapus.php?id=<?= $mhs['id'] ?>">hapus</a> <a href="ubah.php?id=<?= $mhs['id'] ?>">ubah</a></td>
+                <td><img style="width: 50px;" src="../resource/<?= $mhs['gambar'] ?>"></td>
+                <td><?= $mhs['nama'] ?></td>
+                <td><?= $mhs['email'] ?></td>
+                <td><?= $mhs['jurusan'] ?></td>
+                <td><?= $mhs['alamat'] ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+</body>
+
+</html>
